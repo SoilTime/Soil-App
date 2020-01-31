85d2921166caa884aaeb1fc0b3ff0522d3857549
Segun Alesinloye <segunalesh@gmail.com>
2020-01-31 14:21:35 -0600

    added some routes

--- controllers/soilroutes.js
+++ controllers/soilroutes.js
@@ -1,22 +1,20 @@
 var express = require("express");
 var router = express.Router();
-var db = require("../models");
+var db = require("");
 
 module.exports = function (router) {
     router.get('/', function (req, res) {
-        // console.log(db.Student);
-        db.Student.findAll({}).then(data => {
-            // console.log("data", data[0].dataValues);
-            res.render('', {});
-        })
+        res.render('index', {});
+        // db.Student.findAll({}).then(data => {
+        // })
     });
 
    
 
-    router.post("/", function (req, res) {
+    router.post("/:search", function (req, res) {
         console.log(req.body);
 
-        db.Student.create(req.body) //{name:'jkska'}
+        db.Student.create(req.body)
             .then(function (data) {
                 console.log("added in database");
                 res.json(data)
