64d4a4da64825a4b0cbea9e821c9de62de5fb8fc
Jsegel <segeljosh@gmail.com>
2020-02-03 11:45:53 -0600

    test

--- models/wikiapi.js
+++ models/wikiapi.js
@@ -1,3 +1,5 @@
+var axios = require("axios");
+
 var loginUrl = "https://www.mediawiki.org/w/api.php?action=login&lgname=Jsegel12@Project1&lgpassword=n8mqe7fg3ql50genu5iaskushljgicjf&lgtoken=123ABC";
     $.ajax({
         url: loginUrl,
@@ -6,17 +8,23 @@ var loginUrl = "https://www.mediawiki.org/w/api.php?action=login&lgname=Jsegel12
         console.log(response);
     });
 
+axios
+    .get(loginUrl)
+    .then(function(res){
+        console.log(res)
+    }))    
+
     var var2 = queryTerm;
     var2 = var2.toLowerCase()
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
 
-    for (i = 0; i < city2.length; i++) {
-        if (city2[i] == " ") {
-            console.log(city2.replace(/ /g, '%20'));
+    for (i = 0; i < var2.length; i++) {
+        if (var2[i] == " ") {
+            console.log(var2.replace(/ /g, '%20'));
         }
-        console.log(city2);
-        var city3 = city2.replace(/ /g, '%20')
+        console.log(var2);
+        var var3 = var2.replace(/ /g, '%20')
     }
     console.log(city3);
