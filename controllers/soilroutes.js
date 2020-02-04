a267298925fdaeb93278abffadc675166927d358
Jsegel <segeljosh@gmail.com>
2020-02-04 14:59:49 -0600

    adding model and wikipedia api stuff

--- controllers/soilroutes.js
+++ controllers/soilroutes.js
@@ -31,35 +31,56 @@ module.exports = function (router) {
     //         })
     // });
 router.post("/api/coordinates", function(req,res){
-    console.log(req.body)
+    var latMin = req.body.lat - 1;
+    var latMax = parseFloat(req.body.lat) + 1;
+    var lngMax = parseFloat(req.body.lng) + 1;
+    var lngMin = req.body.lng -1;
+    console.log(latMin);
+    console.log(latMax);
+    console.log(lngMax);
+    console.log(lngMin)
     db.Soil.findAll({
-       $and: {
-           Latitude: {
-               $like: req.body.lat
-           },
-           Longitude: {
-               $like: req.body.lng
-           }
-       }
+        where: {
+            Region: "North Carolina"
+        }
+    }).then(function(result){
+        console.log(result[0])
+        var queryTerm = result[0].Species
+        queryTerm2 = queryTerm.toLowerCase()
+        .split(' ')
+        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+        .join(' ');
+        for (i = 0; i < queryTerm2.length; i++) {
+            if (queryTerm2[i] == " ") {
+                // console.log(queryTerm2.replace(/ /g, '%20'));
+            }
+            // console.log(queryTerm2);
+            var queryTerm3 = queryTerm2.replace(/ /g, '%20')
+        }
+        // console.log(queryTerm3)
+        axios.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts|pageimages&format=json&exintro=&titles=" + queryTerm3 + "&piprop=original&format=json")
+        .then(function(res){
+            console.log(res.data.query)
+        })
     })
-    console.log(res)
 })
     
 }
 
 
 
-router.get('/trefle/:plant', function (req, res) {
-    var trefURLKey = "VjlQT2w5L3puRTRJY1JJZ3BETEpPUT09"
-    // var plant = "spinach"
-    axios
-    .get(`https://trefle.io/api/plants?q=${req.params.plant}&token=${trefURLKey}`)
-    .then(function(res) {
-        console.log(res.data)
-    })
+// router.get('/trefle/:plant', function (req, res) {
+//     var trefURLKey = "VjlQT2w5L3puRTRJY1JJZ3BETEpPUT09"
+//     // var plant = "spinach"
+//     axios
+//     .get(`https://trefle.io/api/plants?q=${req.params.plant}&token=${trefURLKey}`)
+//     .then(function(res) {
+//         console.log(res.data)
+//     })
 
-    res.render('soil', {plant: data});
-    });
+//     res.render('soil', {plant: data});
+//     });
     
 
 // module.exports = router;
+
