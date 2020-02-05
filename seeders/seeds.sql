eba1898133279f11551d9a6fc1bf02f8607e6ca9
Segun Alesinloye <segunalesh@gmail.com>
2020-02-05 09:47:14 -0600

    Added readme and new table

---
+++ seeders/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO soil(Country, Latitude, Longitude, Species, Ecosystem_state, Soil_type)
+VALUES("USA", 41.88, -87.63, "Cone Flower", "Open woodlands", "Sandy, Rocky and Clay"),
+("USA", 43.78, -88.79, "Big bluestem", "Meadows", "Sandy or Loamy"),
+("USA", 39.01, -98.48, "Vines", "Forest openings", "Silt loam and clay loam"),
+("USA", 39.77, -86.16, "Black chokeberry", "Rocky upland woods", "Chalk, Clay, Loam, Sand"),
+("USA", 47.75, -120.74, "Western serviceberry", "Rocky, Dry slopes and Well-drained thickets", "Sandy loams and Loams");
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.set('view engine', 'handlebars');
 soilController(app);
 
 
+
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("App listening on: http://localhost:" + PORT);
