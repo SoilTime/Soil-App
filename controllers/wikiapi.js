874ed1ca39a9e25832d48e8b8f1692ae4b50aab1
bonnerac <alex.c.bonner@gmail.com>
2020-03-29 15:28:57 -0500

    cleaned all commented code from wikiapi.js

--- controllers/wikiapi.js
+++ controllers/wikiapi.js
@@ -1,28 +1,27 @@
 var axios = require("axios");
 const address = "test"
 
-// require("../public/assets/js/script")
+
 
 var loginUrl = "https://www.mediawiki.org/w/api.php?action=login&lgname=Jsegel12@Project1&lgpassword=n8mqe7fg3ql50genu5iaskushljgicjf&lgtoken=123ABC";
- axios
+axios
     .get(loginUrl)
-    .then(function(res){
+    .then(function (res) {
         console.log(res)
-    })    
+    })
 
-    var queryTerm = address;
-    queryTerm = queryTerm.toLowerCase()
-        .split(' ')
-        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-        .join(' ');
+var queryTerm = address;
+queryTerm = queryTerm.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
 
-    for (i = 0; i < queryTerm.length; i++) {
-        if (queryTerm[i] == " ") {
-            console.log(queryTerm.replace(/ /g, '%20'));
-        }
-        console.log(queryTerm);
-        var var3 = queryTerm.replace(/ /g, '%20')
+for (i = 0; i < queryTerm.length; i++) {
+    if (queryTerm[i] == " ") {
+        console.log(queryTerm.replace(/ /g, '%20'));
     }
-    console.log(var3);
+    console.log(queryTerm);
+    var var3 = queryTerm.replace(/ /g, '%20')
+}
+console.log(var3);
 
-    // module.exports = var3
